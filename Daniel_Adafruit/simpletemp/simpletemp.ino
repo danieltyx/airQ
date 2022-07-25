239799dc22cc4b13f9cd443ca57959f8461bcede
Yuxuan Tian <danieltian.yx@gmail.com>
2022-07-25 17:05:10 -0400

    725

---
+++ Daniel_Adafruit/simpletemp/simpletemp.ino
@@ -0,0 +1,23 @@
+#include "DHT.h"
+#define DHT11PIN 16
+
+DHT dht(DHT11PIN, DHT11);
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+}
+
+void loop()
+{
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  delay(1000);
+}
